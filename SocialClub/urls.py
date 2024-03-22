a84c7eaf76d115c97c5184050fe89ef8aa4266de
Кирилл <50081172+Cneon90@users.noreply.github.com>
2024-03-23 04:41:31 +0500

    init

---
+++ SocialClub/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import include
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('', include('social_api.urls')),
+]
