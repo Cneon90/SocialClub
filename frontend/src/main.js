63512e0dbbbbc72ae79ecae06fc84853a261aeca
Кирилл <50081172+Cneon90@users.noreply.github.com>
2024-03-23 04:47:44 +0500

    f

---
+++ frontend/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue';
+import App from './App.vue'
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+
+const app = createApp(App)
+
+// app.use(router)
+app.use(Buefy);
+
+app.mount('#app')
