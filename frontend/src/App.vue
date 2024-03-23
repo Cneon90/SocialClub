3f632a1c629737ec66f35b1d3751699e38f9c4ab
Кирилл <50081172+Cneon90@users.noreply.github.com>
2024-03-24 00:19:44 +0500

    init

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,96 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <!-- Меню слева -->
+      <nav class="menu">
+        <MainMenu></MainMenu>
+      </nav>
+
+      <!-- Содержимое страницы -->
+      <div class="content">
+        <!-- Заголовок -->
+        <header>
+          <h1>Добро пожаловать на наш сайт!</h1>
+        </header>
+
+        <div class="contentBox">
+            <div>
+              <h1>Результат запроса1:</h1>
+              <ul>
+                <li v-for="item in items" :key="item.id">{{ item.title }}</li>
+              </ul>
+            </div>
+
+            <section>
+              <b-button @click="clickMe">Click M1e</b-button>
+            </section>
+        </div>
+
+        <!-- Футер -->
+        <footer>
+          <p>&copy; 2022 Название вашей компании. All Rights Reserved.</p>
+        </footer>
+      </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainMenu from "@/components/MainMenu.vue";
+// import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {MainMenu},
+  data() {
+    return {
+      items: []
+    };
+  },
+    mounted() {
+    // axios.get('http://127.0.0.1:8000/users/')
+    //   .then(response => {
+    //     this.items = response.data;
+    //   })
+    //   .catch(error => {
+    //     console.error('Ошибка при загрузке данных:', error);
+    //   });
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  display: flex;
+}
+
+.content {
+  flex: 1;
+  padding: 20px;
+}
+
+header {
+  background-color: #f4f4f4;
+  padding: 10px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.contentBox {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  text-align: center;
+}
+
+footer {
+  margin-top: auto;
+  background-color: #333;
+  color: white;
+  text-align: center;
+  padding: 10px;
 }
 </style>
--- social_api/urls.py
+++ social_api/urls.py
@@ -4,8 +4,11 @@ from rest_framework import routers
 from . import views
 from .views import *
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
-urlpatterns = [
- ]
 
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
